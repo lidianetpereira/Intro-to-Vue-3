9ac42e4a8309ded027f3abd4293bdc20354d5d80
Lidiane Pereira <lidianetpereira26@gmail.com>
2021-09-13 16:07:01 -0300

    Binding href

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            image: 'assets/images/socks_green.jpg'
+            image: 'assets/images/socks_green.jpg',
+            url: 'https://chicorei.com/meia/meia-pagodinho-19384.html'
         }
     }
 })
